0f98995581bb982741a1f21e491f045f66fe7a28
jtown970 <jasonrtowner@gmail.com>
2020-05-19 10:53:29 -0700

    crud api setup

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,7 @@
 const users = require('../data.json')
 
+let id = users[users.length - 1].id + 1
+
 module.exports = {
   getAllUsers: (req, res) => {
     const {email} = req.query
@@ -23,5 +25,54 @@ module.exports = {
     }else {
       res.status(404).send('user not found')
     }
+  },
+
+  creatUser: (req, res) => {
+    const {first_name, last_name, email} = req.body
+
+    const newUser = {id, first_name, last_name, email}
+    users.push(newUser)
+
+    id++
+
+    res.status(200).send(users)
+  },
+
+  editUser: (req, res) => {
+    const {user_id} = req.params
+    const {first_name, last_name, email} = req.body
+
+    const index = users.findIndex(elem => elem.id === +user_id) // need to add a + to make it a number 
+
+    if(index === -1){ // findIndex will return -1 if nothing is found
+      return res.status(404).send('user not found')
+    }
+
+    // const updatedUser = {id: +user_id, first_name, last_name, email}
+
+    const updatedUser = {
+      id: +user_id,
+      first_name: first_name || users[index].first_name,
+      last_name: last_name || users[index].last_name,
+      email: email || users[index].email,
+    }
+
+    users[index] = updatedUser
+
+    res.status(200).send(users)
+  },
+
+  deleteUser: (req, res) => {
+    const {user_id} = req.params
+
+    const index = users.findIndex(e => e.id === +user_id)
+
+    if(index === -1)
+    return res.status(404).send('user not found')
+
+    users.splice(index, 1)
+    
+    res.status(200).send(users)
   }
+
 }
--- server/index.js
+++ server/index.js
@@ -6,15 +6,22 @@ const ctrl = require('./controller')
 
 const SERVER_PORT = 4000      // must be in all caps like its typed never go lower then 3001
 
-app.use(express.json())       
+app.use(express.json())  // make sure to have this
+
+app.use(function(req, res, next){
+  console.log('we got a request cap')
+  next()
+})
 
 app.get('/api/jason', (req, res) => {
   res.status(200).send('Jason')
 })
 
 app.get('/api/users', ctrl.getAllUsers)
-
-app.get('/api/user/:user_id', ctrl.getUserById)
+app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.creatUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () => console.log(`server running on port ${SERVER_PORT}`)) // need to do this to run express and it knows use nodemon server/index.js
 
